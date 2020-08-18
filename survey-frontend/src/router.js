16547cab706c9e2fd18a0d0452fc91af4923dc60
Ikeji Benjamin <benjaminikeji@gmail.com>
2020-08-18 14:30:58 +0100

    Fix routes wrong ref

--- survey-frontend/src/router.js
+++ survey-frontend/src/router.js
@@ -1,6 +1,7 @@
 import { Redirect, Route, Switch, withRouter } from 'react-router-dom';
 import React from 'react';
 import { toast } from 'react-toastify';
+import { get } from 'lodash';
 import routes from './routes';
 import store from './redux/store';
 import { setNextUrl } from './redux/actions';
@@ -10,7 +11,8 @@ import Page404 from './components/route/ErrorPages/Page404';
 export const CustomRoute = customProps => {
   const { component: Component, isPrivate, location, ...rest } = customProps;
   const { auth } = store.getState();
-  const isVerified = !!auth.user && auth.user.account_verified;
+  console.log('auth ', auth);
+  const isVerified = !!auth.user && auth.user.accountVerified;
   const isLoggedIn =
     auth.sessionTimeExpiration &&
     auth.sessionTimeExpiration > new Date().getTime() / 1000;
@@ -18,32 +20,37 @@ export const CustomRoute = customProps => {
     <Route
       {...rest}
       render={props => {
+        const pathname = get(location, 'pathname');
         if (isPrivate && !isLoggedIn) {
-          if (!!location && location.pathname) {
+          if (pathname) {
             store.dispatch(setNextUrl(location.pathname));
           }
           toast.error('You need to be logged in to see this page');
           return <Redirect to="/login" />;
-        } else if (isPrivate && isLoggedIn && !isVerified) {
-          return <Redirect to="/verify-code" />;
+        } else if (isPrivate && !isVerified) {
+          if (pathname && pathname !== '/verify-code') {
+            store.dispatch(setNextUrl(location.pathname));
+            toast.error('Your account needs to be verified first');
+            return <Redirect to="/verify-code" />;
+          }
         }
         return (
-          // <ErrorBoundary>
-          <Component {...props} />
-          // </ErrorBoundary>
+          <ErrorBoundary>
+            <Component {...props} />
+          </ErrorBoundary>
         );
       }}
     />
   );
 };
-const CustomerRouteComponent = withRouter(CustomRoute);
+const CustomRouteComponent = withRouter(CustomRoute);
 export default () => {
   return (
     <Switch>
       {routes.map((props, index) => (
-        <CustomerRouteComponent key={index} {...props} />
+        <CustomRouteComponent key={index} {...props} />
       ))}
-      <CustomerRouteComponent component={Page404} />
+      <CustomRouteComponent component={Page404} />
     </Switch>
   );
 };
--- survey-frontend/src/routes.js
+++ survey-frontend/src/routes.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import Home from './components/route/Home';
 import Login from './components/route/Authentication/Login';
+import Register from './components/route/Authentication/Register';
+import VerifyUser from './components/route/Authentication/Verify/ByLink';
+import VerifyCode from './components/route/Authentication/Verify/ByCode';
+import PasswordRequestEmail from './components/route/Authentication/Password/RequestEmail';
+import resetPassword from './components/route/Authentication/Password/Update';
 
 export default [
   { exact: true, path: '/', component: Home, isPrivate: true },
   { exact: true, path: '/login', component: Login },
+  { exact: true, path: '/register', component: Register },
+  { exact: true, path: '/verify-code', component: VerifyCode, isPrivate: true },
+  { exact: true, path: '/verify-user/:email/:hash', component: VerifyUser },
+  {
+    exact: true,
+    path: '/password/request-email',
+    component: PasswordRequestEmail,
+  },
+  { exact: true, path: '/password/update', component: resetPassword },
 ];
