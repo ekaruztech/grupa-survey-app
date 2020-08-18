89537e43a7135939f879348d3ad12a9cd5a48726
Ikeji Benjamin <benjaminikeji@gmail.com>
2020-08-18 14:30:17 +0100

    Fix routings

--- survey-frontend/src/components/route/Home/index.js
+++ survey-frontend/src/components/route/Home/index.js
@@ -1,15 +1,24 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { connect } from 'react-redux';
 import './Home.scss';
+import { Button } from 'antd';
+import { logout } from '../../../redux/actions';
 
 const Home = props => {
-  const { auth } = props;
+  const { auth, logout } = props;
 
   useEffect(() => {
     return () => {};
   }, []);
 
-  return <div>Hello home</div>;
+  return (
+    <div style={{ width: '400px', textAlign: 'center', margin: '50px auto' }}>
+      <h2>Welcome home</h2>
+      <Button onClick={logout} type="link">
+        Logout
+      </Button>
+    </div>
+  );
 };
 const mapStateToProps = state => {
   return {
@@ -17,6 +26,6 @@ const mapStateToProps = state => {
   };
 };
 
-const dispatchToProps = {};
+const dispatchToProps = { logout };
 
 export default connect(mapStateToProps, dispatchToProps)(Home);
