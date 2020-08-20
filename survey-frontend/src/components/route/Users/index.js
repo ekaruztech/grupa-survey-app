a69221e21dc95f3c8741869fbec2820f980c7efd
Cmion <akpanudosimeon@hotmail.com>
2020-08-20 01:21:08 +0100

    WIP: implementing survey response slides

--- survey-frontend/src/components/route/Users/index.js
+++ survey-frontend/src/components/route/Users/index.js
@@ -1,119 +1,121 @@
-import React, { useState } from 'react';
-import { Form, Button, Checkbox, Row, Col } from 'antd';
+import React, { useState, useRef } from 'react';
+import {
+  Layout,
+  Carousel,
+  PageHeader,
+  Button,
+  Row,
+  Col,
+  Statistic,
+} from 'antd';
 import './users.scss';
-import { ArrowLeftOutlined } from '@ant-design/icons';
-
-const formTailLayout = {
-  labelCol: {
-    span: 4,
-  },
-  wrapperCol: {
-    span: 8,
-    offset: 4,
-  },
-};
+import {
+  LeftOutlined,
+  RightOutlined,
+  ReadOutlined,
+  NumberOutlined,
+} from '@ant-design/icons';
+import { motion } from 'framer-motion';
+import { Align, Padding } from '../Authentication/_common/components';
+import Question from './_commons/Question';
 
 const Users = () => {
-  const [checkOption, setcheckOption] = useState(false);
-  const [checkOption2, setcheckOption2] = useState(false);
-  const [checkOption3, setcheckOption3] = useState(false);
-  const [checkOption4, setcheckOption4] = useState(false);
-  const [checkOption5, setcheckOption5] = useState(false);
-  const [size, setSize] = useState('large');
-  const onCheckboxChange1 = e => {
-    setcheckOption(e.target.checked);
-  };
+  const onChange = value => console.log(value);
+  const questionsRef = useRef();
+  const questions = Array(10).fill(10);
+  const [currentSlide, setCurrentSlide] = useState(1);
 
-  const onCheckboxChange2 = e => {
-    setcheckOption2(e.target.checked);
-  };
-
-  const onCheckboxChange3 = e => {
-    setcheckOption3(e.target.checked);
-  };
-
-  const onCheckboxChange4 = e => {
-    setcheckOption4(e.target.checked);
-  };
-
-  const onCheckboxChange5 = e => {
-    setcheckOption5(e.target.checked);
-  };
   return (
-    <Row justify={'center'} align={'middle'} className="users-layout">
-      <Col>
-        <Form
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={false}
-          onFinishFailed={false}
-        >
-          <div className="bg-white p-4">
-            <Button
-              className="prevPosition"
-              type="primary"
-              icon={<ArrowLeftOutlined />}
-              onClick={false}
-              size={size}
+    <Layout style={{ overflow: 'hidden' }}>
+      <PageHeader
+        ghost={false}
+        onBack={() => window.history.back()}
+        title="Randomized quizzes"
+      >
+        <Align alignCenter justifyBetween>
+          <Padding right={10}>
+            <Button icon={<ReadOutlined />} type={'link'}>
+              7 questions
+            </Button>
+          </Padding>
+          <Align alignCenter>
+            <Padding right={10} key={'question-title'}>
+              Question
+            </Padding>
+            <Statistic
+              key={'question-count'}
+              value={currentSlide}
+              suffix={`/ ${questions.length}`}
             />
-            <h1 className="users-h1">
-              How many players won gold medal at the 2020 Olympics?
-            </h1>
-            <Form.Item className="ant-form-item-hover">
-              <Checkbox checked={checkOption} onChange={onCheckboxChange1}>
-                Option 1
-              </Checkbox>
-            </Form.Item>
-            <Form.Item className="ant-form-item-hover">
-              <Checkbox checked={checkOption2} onChange={onCheckboxChange2}>
-                Option 2
-              </Checkbox>
-            </Form.Item>
-            <Form.Item className="ant-form-item-hover">
-              <Checkbox checked={checkOption3} onChange={onCheckboxChange3}>
-                Option 3
-              </Checkbox>
-            </Form.Item>
-            <Form.Item className="ant-form-item-hover">
-              <Checkbox checked={checkOption4} onChange={onCheckboxChange4}>
-                Option 4
-              </Checkbox>
-            </Form.Item>
-            <Form.Item className="ant-form-item-hover">
-              <Checkbox checked={checkOption5} onChange={onCheckboxChange5}>
-                Option 5
-              </Checkbox>
-            </Form.Item>
-          </div>
-
-          <div>
-            <Form.Item>
-              <Button
-                type="primary"
-                size={size}
-                htmlType="button"
-                className="nextbtn"
-                style={{
-                  padding: '0 50px',
-                  height: '50px',
-                }}
-                onClick={false}
-              >
-                Next
-              </Button>
-            </Form.Item>
-          </div>
-          {/* <div className="align-center mt-2">
-            <Form.Item>
-              <Button size={size} type="primary">
-                SUBMIT SURVEY
-              </Button>
-            </Form.Item>
-          </div> */}
-        </Form>
-      </Col>
-    </Row>
+          </Align>
+        </Align>
+      </PageHeader>
+      <Row>
+        <Col span={24}>
+          <Carousel afterChange={onChange} dots={false} ref={questionsRef}>
+            {questions.map((value, index) => {
+              return (
+                <Question
+                  key={index}
+                  className={'carousel-form'}
+                  span={24}
+                  offset={0}
+                  question={{
+                    label: 'Who is the first president of Nigeria',
+                    description: 'Whatever you do, do good',
+                    options: [
+                      { label: 'Lauriel Michel', value: 1 },
+                      { label: 'Emmanuel Okafor', value: 11 },
+                      { label: 'Emmanuel Jackson', value: 111 },
+                      { label: 'Abraham LinkedIn', value: 1111 },
+                    ],
+                  }}
+                />
+              );
+            })}
+          </Carousel>
+        </Col>
+        <Col span={24}>
+          <Padding top={30}>
+            <Align
+              alignCenter
+              justifyBetween={
+                currentSlide >= 2 && currentSlide < questions?.length
+              }
+              justifyEnd={currentSlide < 2}
+            >
+              {currentSlide >= 2 && (
+                <motion.div layout>
+                  {' '}
+                  <Button
+                    icon={<LeftOutlined />}
+                    onClick={() => {
+                      setCurrentSlide(prev => prev - 1);
+                      questionsRef.current?.prev?.();
+                    }}
+                  >
+                    Previous
+                  </Button>
+                </motion.div>
+              )}
+              {currentSlide < questions.length && (
+                <motion.div layout>
+                  <Button
+                    type={'primary'}
+                    onClick={() => {
+                      setCurrentSlide(prev => prev + 1);
+                      questionsRef.current?.next?.();
+                    }}
+                  >
+                    Next <RightOutlined />
+                  </Button>
+                </motion.div>
+              )}
+            </Align>
+          </Padding>
+        </Col>
+      </Row>
+    </Layout>
   );
 };
 
