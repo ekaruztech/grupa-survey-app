7254fa128890e20475031d9e689e7a41946f6393
John Lamar <okaforjohnekene316@gmail.com>
2020-08-20 08:39:49 +0100

    Home route

---
+++ survey-frontend/src/components/route/Home/_common/DisplayCard/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Col, Button } from 'antd';
+import { Align, Padding } from '../../../Authentication/_common/components';
+
+const Index = ({ surveyQuestion }) => {
+  return (
+    <Col span={4}>
+      <Padding style={{ height: '100%' }}>
+        <Align
+          style={{ height: 250 }}
+          alignCenter
+          justifyCenter
+          type={'column'}
+        >
+          <h1 className="text0">New website</h1>
+          <h4 className="text1">New website feedback survey</h4>
+          <p className="text2">{surveyQuestion.length} Questions</p>
+        </Align>
+        <Align
+          style={{
+            height: 50,
+            borderTop: '1px solid var(--border-default-color)',
+          }}
+          alignCenter
+          justifyBetween
+        >
+          <Button
+            block
+            type={'primary'}
+            onClick={false}
+            style={{ height: 50, borderRadius: 0 }}
+          >
+            Take Survey
+          </Button>
+        </Align>
+      </Padding>
+    </Col>
+  );
+};
+
+export default Index;
